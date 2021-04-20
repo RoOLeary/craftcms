087299d5bc495747517dc4470cb94d9b38469613
Ronan <roleary81@gmail.com>
2021-04-20 09:18:32 +0200

    hmmmm

--- scripts/run.sh
+++ scripts/run.sh
@@ -18,7 +18,7 @@ activate_plugins
 
 wait
 
-h2 "✅  Visit http://localhost or http://<docker-machine-ip> to start using Craft CMS."
+h2 "✅  Visit http://localhost:5000 or http://<docker-machine-ip> to start using Craft CMS."
 
 # Start php-fpm
 exec "$@"
